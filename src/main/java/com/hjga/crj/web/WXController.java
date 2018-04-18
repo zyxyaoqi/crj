f324294469e1015164914f6952a3c0828df68d90
zyxyaoqi <apple@appledeMacBook-Pro.local>
2018-04-18 18:07:55 +0800

    3

--- src/main/java/com/hjga/crj/web/WXController.java
+++ src/main/java/com/hjga/crj/web/WXController.java
@@ -5,24 +5,28 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/wx/")
+@RequestMapping("/")
 public class WXController {
 
 	@Value("${wx.token}")
 	private String token;
 	
-	@RequestMapping(value = "/",method=RequestMethod.GET)
-    public String checkName(@RequestParam(name="signature")String signature,
-                            @RequestParam(name="timestamp")String timestamp,
-                            @RequestParam(name="nonce")String nonce,
-                            @RequestParam(name="echostr")String echostr){
-        System.out.println("-----------------------开始校验------------------------");
+	@RequestMapping(value = "/wx",method=RequestMethod.GET)
+    public String checkName(@RequestParam(name="signature", defaultValue="")String signature,
+                            @RequestParam(name="timestamp", defaultValue="")String timestamp,
+                            @RequestParam(name="nonce", defaultValue="")String nonce,
+                            @RequestParam(name="echostr", defaultValue="")String echostr){
+        
+		
+		if(StringUtils.isEmpty(signature) || StringUtils.isEmpty(timestamp) ||StringUtils.isEmpty(nonce)) 
+			return "authentication is failed";
         //排序
         String sortString = sort(token, timestamp, nonce);
         //加密
@@ -30,7 +34,6 @@ public class WXController {
         //校验
         if (myString != null && myString != "" && myString.equals(signature)) {
             System.out.println("签名校验通过");
-            //如果检验成功原样返回echostr，微信服务器接收到此输出，才会确认检验完成。
             return echostr;
         } else {
             System.out.println("签名校验失败");
