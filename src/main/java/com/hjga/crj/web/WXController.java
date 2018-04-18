17f917178814f50ec736b444d08de44e1a42e247
zyxyaoqi <apple@appledeMacBook-Pro.local>
2018-04-18 17:59:23 +0800

    2

--- src/main/java/com/hjga/crj/web/WXController.java
+++ src/main/java/com/hjga/crj/web/WXController.java
@@ -43,12 +43,8 @@ public class WXController {
     public String sort(String token, String timestamp, String nonce) {
         String[] strArray = {token, timestamp, nonce};
         Arrays.sort(strArray);
-        StringBuilder sb = new StringBuilder();
-        for (String str : strArray) {
-            sb.append(str);
-        }
-
-        return sb.toString();
+        String content = strArray[0].concat(strArray[1]).concat(strArray[2]);
+        return content;
     }
 
     /**
@@ -58,25 +54,39 @@ public class WXController {
      * @return 加密后的内容
      */
     public String sha1(String str) {
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-1");
-            digest.update(str.getBytes());
-            byte messageDigest[] = digest.digest();
-            // Create Hex String
-            StringBuffer hexString = new StringBuffer();
-            // 字节数组转换为 十六进制 数
-            for (int i = 0; i < messageDigest.length; i++) {
-                String shaHex = Integer.toHexString(messageDigest[i] & 0xFF);
-                if (shaHex.length() < 2) {
-                    hexString.append(0);
-                }
-                hexString.append(shaHex);
-            }
-            return hexString.toString();
-
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+    	 String checktext = null;
+    	 try {
+             MessageDigest md = MessageDigest.getInstance("SHA-1");
+             //对接后的字符串进行sha1加密
+             byte[] digest = md.digest(str.toString().getBytes());
+             checktext = byteToStr(digest);
+         } catch (NoSuchAlgorithmException e){
+             e.printStackTrace();
+         }
+    	//将加密后的字符串与signature进行对比
+         return checktext;
+     }
+    
+    private static String byteToStr(byte[] byteArrays){
+        String str = "";
+        for (int i = 0; i < byteArrays.length; i++) {
+            str += byteToHexStr(byteArrays[i]);
         }
-        return "";
+        return str;
     }
+    
+    /**
+     *  将字节转化为十六进制字符串
+     * @param myByte 字节
+     * @return 字符串
+     */
+    private static String byteToHexStr(byte myByte) {
+        char[] Digit = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        char[] tampArr = new char[2];
+        tampArr[0] = Digit[(myByte >>> 4) & 0X0F];
+        tampArr[1] = Digit[myByte & 0X0F];
+        String str = new String(tampArr);
+        return str;
+    }
+
 }
